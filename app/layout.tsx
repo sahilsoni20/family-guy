32908c8a0e94a26824213f476cec1d777b2e1578
sahilsoni20 <ssahil3120@gmail.com>
2024-07-05 15:20:00 +0530

    ✨ Initial init

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" type="image/svg+xml" href="/logo.png" />
+      </head>
       <body className={inter.className}>
         <Navigation />
         {children}
