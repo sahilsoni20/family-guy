c023edbcb8a1b765db812d392c5957b4acccbaa0
sahilsoni20 <ssahil3120@gmail.com>
2024-07-05 17:10:24 +0530

    endpoint: quiz ques

--- lib/quiz.ts
+++ lib/quiz.ts
@@ -1,5 +1,6 @@
 import { endpoint } from "@/utils/endpoint";
 
+//we are using no-store, by which if user enters again in website the same question will not shown, as it will not store any of the cache 
 export async function getRadomQuizQuestion() {
     const data = await fetch(`${endpoint}/quiz/random`, {cache: 'no-store'})
 
