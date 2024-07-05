ad6548aba74f6a5a600d5f3635f6a50c929037ae
sahilsoni20 <ssahil3120@gmail.com>
2024-07-05 17:07:31 +0530

    ✨ Initial init

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
-import { Container } from "@/components/Container"
-import { getAllCharacters } from "@/lib/characters"
-import Image from "next/image"
-import Link from "next/link"
-
+import { Container } from "@/components/Container";
+import { getAllCharacters } from "@/lib/characters";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function HomePage() {
-  const data = await getAllCharacters()
+  const data = await getAllCharacters();
 
   return (
     <main>
-       <Container className="px-5 w-full max-w-screen-md m-auto grid grid-cols-2 gap-1 py-5 md:grid-cols-3 lg:grid-cols-4">
-        {data?.characters?.map(item => {
+      <Container className="px-5 w-full max-w-screen-md m-auto grid grid-cols-2 gap-1 py-5 md:grid-cols-3 lg:grid-cols-4">
+        {data?.characters?.map((item) => {
           return (
             <Link
               href={`/characters/${item.slug}`}
@@ -25,9 +24,9 @@ export default async function HomePage() {
                 className="transition-all duration-500 hover:scale-110 hover:-rotate-2"
               />
             </Link>
-          )
+          );
         })}
       </Container>
     </main>
-  )
+  );
 }
